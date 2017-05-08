cc1404e4cc69d9bdb4103b51127672d636b24730
Btib <stryder994@hotmail.com>
2017-05-08 19:22:23 +0200

    Frissített home, kész popup, egyszerű kijelentkezés

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,37 @@ import { Router } from '@angular/router';
   templateUrl: `home.component.html`,
 })
 export class HomeComponent  { 
- 
+  newAdventureShow: boolean;
+  adventureTitle: string;
+  partySize: number;
+  minLvl: number;
+  maxLvl: number;
+
+  constructor(private router: Router) {
+    this.newAdventureShow = false;
+  }
+
+  initiateLogout(){
+    this.router.navigate(['/login']);
+  }
+
+  closeNewAdv(){
+    this.newAdventureShow = false;
+    this.adventureTitle = undefined;
+    this.partySize = undefined;
+    this.minLvl = undefined;
+    this.maxLvl = undefined;
+  }
+  showNewAdventurePopup(){
+    this.newAdventureShow = true;
+  }
+  confirmNewAdventure(){
+    if(this.adventureTitle == undefined || this.adventureTitle == ""|| this.partySize == undefined || this.partySize < 1 ||  this.minLvl == undefined || this.minLvl < 1 || this.maxLvl == undefined || this.maxLvl < 1 ){
+      alert("Kérlek add meg az összes infót!");
+      return false;
+    } 
+    console.log(this.adventureTitle + " " + this.partySize + " " + this.minLvl + " " + this.maxLvl);
+    this.closeNewAdv();
+  }
 
 }
